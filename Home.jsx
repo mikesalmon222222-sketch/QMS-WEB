d6c7f1a4270a2bbcc417cb07f59a8ae9746b321d
mikesalmon222222-sketch <mikesalmon222222@gmail.com>
2025-09-29 15:45:10 -0600

    Updated margin-bottom for Reliable Procurement title

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ marginBottom: '10px' }}>Reliable Procurement</h1>
+      {/* Other components/buttons below */}
+      <button>Button 1</button>
+      <button>Button 2</button>
+    </div>
+  );
+};
+
+export default Home;
